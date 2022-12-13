10202fdc69e9f4ebbec61b19b2ed4d7bb7bf4618
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2022-12-13 18:44:33 +0530

    Add user registration

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("", include("django.contrib.auth.urls")),
+    path("register/", views.Register.as_view(), name="register"),
+    path(
+        "success/", views.registeration_success, name="registeration_success"
+    ),
 ]
