27c450387f2be4c2b86448c231792c14c08cd0ca
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2022-12-13 18:44:33 +0530

    Add user registration

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,8 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 
 @login_required
 def dashboard(request):
     return render(request, "account/dashboard.html", {"section": "dashboard"})
+
+
+class Register(CreateView):
+    form_class = UserRegistrationForm
+    template_name = "account/register.html"
+    success_url = reverse_lazy("registeration_success")
+
+
+def registeration_success(request):
+    return render(request, "account/register_done.html")
