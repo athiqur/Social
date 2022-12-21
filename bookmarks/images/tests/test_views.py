417d8b0770208087a4d2d8f8913bc439dfca0dfa
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2022-12-21 13:48:50 +0530

    Add image like option

--- bookmarks/images/tests/test_views.py
+++ bookmarks/images/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from images.tests.test_modelmixin import ModelMixin
 from django.urls import reverse
-
+from images.models import Image
 
 class TestDetailView(ModelMixin, TestCase):
     def test_detail_view_returns_to_success_view(self):
@@ -24,3 +24,72 @@ class TestDetailView(ModelMixin, TestCase):
             reverse("images:detail", args=[1, "rehman"])
         )
         self.assertEquals(response.status_code, 404)
+
+
+class TestImageLikeView(ModelMixin, TestCase):
+    def test_image_like_view_succeeds_adding_like_to_the_image(self):
+        self.client.login(**self.credentials)
+        self.client.post(
+            reverse("images:create"),
+            {
+                "title": "rehman",
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Berlin_Opera_UdL_asv2018-05.jpg/800px-Berlin_Opera_UdL_asv2018-05.jpg",
+            },
+        )
+        self.client.get(
+            reverse(
+                "images:detail",
+                args=[Image.objects.first().pk, Image.objects.first().slug],
+            )
+        )
+        response = self.client.post(
+            reverse("images:like"),
+            {"id": Image.objects.first().pk, "action": "like"},
+        )
+        self.assertJSONEqual(response.content, {"status": "ok"})
+
+    def test_image_like_view_fails_adding_like_or_unlike_to_the_image(self):
+        self.client.login(**self.credentials)
+        self.client.post(
+            reverse("images:create"),
+            {
+                "title": "rehman",
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Berlin_Opera_UdL_asv2018-05.jpg/800px-Berlin_Opera_UdL_asv2018-05.jpg",
+            },
+        )
+        self.client.get(
+            reverse(
+                "images:detail",
+                args=[Image.objects.first().pk, Image.objects.first().slug],
+            )
+        )
+        response = self.client.post(
+            reverse("images:like"),
+            {"id": Image.objects.first().pk, "action": "None"},
+        )
+        self.assertJSONEqual(response.content, {"status": "error"})
+
+    def test_image_like_view_succeeds_removing_like_from_the_image(self):
+        self.client.login(**self.credentials)
+        self.client.post(
+            reverse("images:create"),
+            {
+                "title": "rehman",
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Berlin_Opera_UdL_asv2018-05.jpg/800px-Berlin_Opera_UdL_asv2018-05.jpg",
+            },
+        )
+        self.client.get(
+            reverse(
+                "images:detail",
+                args=[Image.objects.first().pk, Image.objects.first().slug],
+            )
+        )
+        self.client.post(
+            reverse("images:like"),
+            {"id": Image.objects.first().pk, "action": "like"},
+        )
+        response = self.client.post(
+            reverse("images:like"),
+            {"id": Image.objects.first().pk, "action": "unlike"},
+        )
+        self.assertJSONEqual(response.content, {"status": "ok"})
