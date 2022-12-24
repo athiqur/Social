3588da365463ab927610006255c11e95dde77217
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2022-12-25 01:22:19 +0530

    Refactor:indentation

--- bookmarks/images/static/js/bookmarklet.js
+++ bookmarks/images/static/js/bookmarklet.js
@@ -6,7 +6,6 @@
     var min_height = 100;
 
     function bookmarklet(msg) {
-        // load CSS
         var css = jQuery('<link>');
         css.attr({
             rel: 'stylesheet',
@@ -15,15 +14,12 @@
         });
         jQuery('head').append(css);
 
-        // load HTML
         box_html = '<div id="bookmarklet"><a href="#" id="close">&times;</a><h1>Select an image to bookmark:</h1><div class="images"></div></div>';
         jQuery('body').append(box_html);
 
-        // close event
         jQuery('#bookmarklet #close').click(function () {
             jQuery('#bookmarklet').remove();
         });
-        // find images and display them
         jQuery.each(jQuery('img[src$="jpg"]'), function (index, image) {
             if (jQuery(image).width() >= min_width && jQuery(image).height()
                 >= min_height) {
@@ -33,12 +29,9 @@
             }
         });
 
-        // when an image is selected open URL with it
         jQuery('#bookmarklet .images a').click(function (e) {
             selected_image = jQuery(this).children('img').attr('src');
-            // hide bookmarklet
             jQuery('#bookmarklet').hide();
-            // open new window to submit the image
             window.open(site_url + 'images/create/?url='
                 + encodeURIComponent(selected_image)
                 + '&title='
@@ -48,28 +41,20 @@
 
     };
 
-    // Check if jQuery is loaded
     if (typeof window.jQuery != 'undefined') {
         bookmarklet();
     } else {
-        // Check for conflicts
         var conflict = typeof window.$ != 'undefined';
-        // Create the script and point to Google API
         var script = document.createElement('script');
         script.src = '//ajax.googleapis.com/ajax/libs/jquery/' +
             jquery_version + '/jquery.min.js';
-        // Add the script to the 'head' for processing
         document.head.appendChild(script);
-        // Create a way to wait until script loading
         var attempts = 15;
         (function () {
-            // Check again if jQuery is undefined
             if (typeof window.jQuery == 'undefined') {
                 if (--attempts > 0) {
-                    // Calls himself in a few milliseconds
                     window.setTimeout(arguments.callee, 250)
                 } else {
-                    // Too much attempts to load, send error
                     alert('An error occurred while loading jQuery')
                 }
             } else {
