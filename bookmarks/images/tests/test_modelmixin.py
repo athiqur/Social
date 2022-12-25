db82274e568c5a353145560e871e6912b94b9e2e
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2022-12-25 23:40:28 +0530

    Refactor:testcases

--- bookmarks/images/tests/test_modelmixin.py
+++ bookmarks/images/tests/test_modelmixin.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from account.models import Profile
 
 
 class ModelMixin(TestCase):
@@ -9,3 +10,4 @@ class ModelMixin(TestCase):
             "password": "123",
         }
         self.user = User.objects.create_user(**self.credentials)
+        Profile.objects.create(user=self.user)
