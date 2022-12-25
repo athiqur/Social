ebded486b61afaa8bf68296d375306e83edea887
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2022-12-25 23:06:52 +0530

    Add testcases

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 
 app_name = "images"
@@ -8,5 +9,5 @@ urlpatterns = [
     path("create/", views.image_create, name="create"),
     path("detail/<int:id>/<slug:slug>/", views.image_detail, name="detail"),
     path("like/", views.image_like, name="like"),
-    path("", views.ImageListView.as_view(), name="list"),
+    path("", login_required(views.ImageListView.as_view()), name="list"),
 ]
