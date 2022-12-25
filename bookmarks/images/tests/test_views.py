626c495205fff50f96db0aa45bd517153dc92611
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2022-12-25 23:06:52 +0530

    Add testcases

--- bookmarks/images/tests/test_views.py
+++ bookmarks/images/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from images.tests.test_modelmixin import ModelMixin
 from django.urls import reverse
 
+
 class TestDetailView(ModelMixin, TestCase):
     def test_detail_view_returns_to_success_view(self):
         self.client.login(**self.credentials)
@@ -17,7 +18,7 @@ class TestDetailView(ModelMixin, TestCase):
         )
         self.assertEquals(response.status_code, 404)
 
-        
+
 class TestImageLikeView(ModelMixin, TestCase):
     def test_image_like_view_succeeds_adding_like_to_the_image_when_called_with_ajax(
         self,
@@ -35,7 +36,6 @@ class TestImageLikeView(ModelMixin, TestCase):
             **{"HTTP_X_REQUESTED_WITH": "XMLHttpRequest"}
         )
         self.assertJSONEqual(response.content, {"status": "ok"})
-
 
     def test_image_like_view_fails_when_the_action_is_invalid(self):
         self.client.login(**self.credentials)
@@ -73,3 +73,22 @@ class TestImageLikeView(ModelMixin, TestCase):
             **{"HTTP_X_REQUESTED_WITH": "XMLHttpRequest"}
         )
         self.assertJSONEqual(response.content, {"status": "ok"})
+
+
+class TestImageListView(ModelMixin, TestCase):
+    def test_image_list_view_uses_correct_template_when_the_request_is_ajax(
+        self,
+    ):
+        self.client.login(**self.credentials)
+        response = self.client.get(
+            reverse("images:list"),
+            **{"HTTP_X_REQUESTED_WITH": "XMLHttpRequest"}
+        )
+        self.assertTemplateUsed(response, "images/image/list_ajax.html")
+
+    def test_image_list_view_uses_correct_template_when_the_request_is_not_ajax(
+        self,
+    ):
+        self.client.login(**self.credentials)
+        response = self.client.get(reverse("images:list"))
+        self.assertTemplateUsed(response, "images/image/list.html")
