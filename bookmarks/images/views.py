de60286ce35cd88017398fb174386099e4b6f354
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2022-12-27 18:59:58 +0530

    Add create action helper function in images app and accounts app

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -10,6 +10,7 @@ from common.decorators import ajax_required, is_ajax
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from actions.utils import create_action
 
 
 @login_required
@@ -19,6 +20,7 @@ def image_create(request):
         new_item = form.save(commit=False)
         new_item.user = request.user
         new_item.save()
+        create_action(request.user, "bookmarked image", new_item)
         messages.success(request, "Image added successfully")
         return redirect(new_item.get_absolute_url())
 
@@ -48,6 +50,7 @@ def image_like(request):
         image = Image.objects.get(id=image_id)
         if action == "like":
             image.users_like.add(request.user)
+            create_action(request.user, "likes", image)
             return JsonResponse({"status": "ok"})
         elif action == "unlike":
             image.users_like.remove(request.user)
