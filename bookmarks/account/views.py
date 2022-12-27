dc2e8fa63f6919fcf33645c0eddcc87b96cfa084
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2022-12-27 15:40:58 +0530

    Refactor:views

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -94,20 +94,18 @@ def user_detail(request, username):
 def user_follow(request):
     user_id = request.POST.get("id")
     action = request.POST.get("action")
-    if user_id and action:
-        try:
-            user = User.objects.get(id=user_id)
-            if action == "follow":
-                Contact.objects.get_or_create(
-                    user_from=request.user, user_to=user
-                )
-            else:
-                Contact.objects.filter(
-                    user_from=request.user, user_to=user
-                ).delete()
+    try:
+        user = User.objects.get(id=user_id)
+        if action == "follow":
+            Contact.objects.get_or_create(user_from=request.user, user_to=user)
+            return JsonResponse({"status": "ok"})
+        elif action == "unfollow":
+            Contact.objects.filter(
+                user_from=request.user, user_to=user
+            ).delete()
             return JsonResponse({"status": "ok"})
 
-        except User.DoesNotExist:
-            return JsonResponse({"status": "error"})
+    except User.DoesNotExist:
+        return JsonResponse({"status": "error"})
 
     return JsonResponse({"status": "error"})
