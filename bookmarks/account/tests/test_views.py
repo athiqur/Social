53cb2c79ba3a11b3c9163cbd81ff8a71941181e1
athiqur <116162887+athiqur@users.noreply.github.com>
2023-01-02 10:57:29 +0530

    Create logout view (#2)

--- bookmarks/account/tests/test_views.py
+++ bookmarks/account/tests/test_views.py
@@ -15,3 +15,19 @@ class TestUserLogin(ModelMixinTestCase, TestCase):
         self.client.login(**self.credentials)
         response = self.client.get(reverse("social:dashboard"), follow=True)
         self.assertTemplateUsed(response, "account/dashboard.html")
+
+
+class TestLogoutView(ModelMixinTestCase, TestCase):
+    def test_auth_logout_view_uses_correct_template(self):
+        self.assertTemplateUsed(
+            self.client.get(reverse("social:logout")),
+            "registration/logged_out.html",
+        )
+
+    def test_auth_logout_view_uses_correct_template_after_successful_logout(
+        self,
+    ):
+        self.client.login(**self.credentials)
+        self.client.logout
+        response = self.client.get(reverse("social:dashboard"), follow=True)
+        self.assertTemplateUsed(response, "account/dashboard.html")
