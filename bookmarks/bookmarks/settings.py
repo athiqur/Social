ed412c6795f62da8e2d4714814949433869c75f6
athiqur <116162887+athiqur@users.noreply.github.com>
2023-01-02 11:11:41 +0530

    Register image model to the admin site (#11)
    * Register image model to admin site
    
    * Refactor:models
    
    * Refactor:settings
    
    Co-authored-by: Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "account.apps.AccountConfig",
+    "images.apps.ImagesConfig" "account.apps.AccountConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
